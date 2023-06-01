1cd9aeaceb5bf1134d3d92cbf8deabac7585e4ba
Gustavo Arias <ariasgustavo3a@gmail.com>
2023-06-01 15:37:28 -0300

    intento de slide fallido

--- TPE2/js/nav.js
+++ TPE2/js/nav.js
@@ -1,5 +1,6 @@
 "use strict"
 
+/* ---- Comienzo del nav ----*/
 const menu = document.querySelector("#nav-links");
 const openMenuBtn = document.querySelector("#btn-hambur");
 const closeMenuBtn = document.querySelector("#btn-cross");
@@ -10,3 +11,42 @@ function mostrarMenu() {
 
 openMenuBtn.addEventListener("click", mostrarMenu);
 closeMenuBtn.addEventListener("click", mostrarMenu);
+/* ---- FIN del nav ----*/
+
+
+
+
+/* ---- comienxo del slide ----*/
+
+let arregloImg = ["img1.jpg","img2.jpg","img3.jpg","img4.jpg","img5.jpg","img6.jpg","img7.jpg"];
+let imgActual = 0;
+
+//setear tiempo
+let tempo;
+tempo = setInterval(pasarImagen, 2000);
+document.querySelector('#btn-sig').addEventListener("click" , pasarImagen);
+document.querySelector('#btn-ant').addEventListener("click" , pasarImagen);
+let contImgSlide = document.querySelector('.img-slide');
+contImgSlide.addEventListener("mouseover", detenerSlide);
+contImgSlide.addEventListener("mouseout", arrancarSlide);
+
+function pasarImagen() {
+  let boton= this.id;
+  if(boton!=undefined){
+    clearInterval(pasarImagen,2000);
+    tempo = setInterval(pasarImagen, 2000);
+  }
+  if (boton=='btn-sig') {
+    imgActual++;
+    if(imgActual==arregloImg.length()){
+      imgActual = 0;
+    }
+  } else {
+    imgActual--;
+    if (imgActual<0) {
+      imgActual=arregloImg.length()-1;
+    }
+  }
+  let showImg = arregloImg[imgActual];
+  contImgSlide.scr = `img/${showImg}`;
+}
