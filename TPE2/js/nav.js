c2c567f2276b8986810f2925065165b4eff04504
Gustavo Arias <ariasgustavo3a@gmail.com>
2023-06-01 13:51:05 -0300

    menu nav andando, retocar margenes

--- TPE2/js/nav.js
+++ TPE2/js/nav.js
@@ -1,2 +1,12 @@
-let boton_nav = document.querySelector("#btn-mob-menu");
+"use strict"
 
+const menu = document.querySelector("#nav-links");
+const openMenuBtn = document.querySelector("#btn-hambur");
+const closeMenuBtn = document.querySelector("#btn-cross");
+
+function mostrarMenu() {
+  menu.classList.toggle("menu_abierto");
+}
+
+openMenuBtn.addEventListener("click", mostrarMenu);
+closeMenuBtn.addEventListener("click", mostrarMenu);
