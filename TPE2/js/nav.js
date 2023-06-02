9cfbe4fa1291abb8c40670f51a037a41b155dce8
Gustavo Arias <ariasgustavo3a@gmail.com>
2023-06-01 21:43:34 -0300

    menu responsive ok slider ok, buscar imgs para poner

--- TPE2/js/nav.js
+++ TPE2/js/nav.js
@@ -18,37 +18,47 @@ closeMenuBtn.addEventListener("click", mostrarMenu);
 
 /* ---- comienxo del slide ----*/
 
-let arregloImg = ["img1.jpg","img2.jpg","img3.jpg","img4.jpg","img5.jpg","img6.jpg","img7.jpg"];
+let arregloImg = ["img0.jpg","img1.jpg","img2.jpg","img3.jpg","img4.jpg","img5.jpg","img6.jpg"];
 let imgActual = 0;
 
 //setear tiempo
 let tempo;
-// tempo = setInterval(pasarImagen, 2000);
-document.querySelector('#btn-sig').addEventListener("click" , ()=>pasarImagen(1));
+tempo = setInterval(pasarImagen, 5000);
+
+document.querySelector('#btn-sig').addEventListener("click" , pasarImagen);
 document.querySelector('#btn-ant').addEventListener("click" , pasarImagen);
 let contImgSlide = document.querySelector('#img-slide');
-// contImgSlide.addEventListener("mouseover", detenerSlide);
-// contImgSlide.addEventListener("mouseout", arrancarSlide);
 
-function pasarImagen(n) {
-  // let boton= this.id;
-  imgActual+=n;
-  imgActual=imgActual%3+1;
-  let nombreImg = "img/img"+imgActual+".jpg";
-  console.log(nombreImg);
-  contImgSlide.style = "background: url("+nombreImg+");";
+contImgSlide.addEventListener("mouseover", detenerSlide);
+contImgSlide.addEventListener("mouseout", arrancarSlide);
 
-  // if (boton=='btn-sig') {
-  //   imgActual++;
-  //   if(imgActual==arregloImg.length()){
-  //     imgActual = 0;
-  //   }
-  // } else {
-  //   imgActual--;
-  //   if (imgActual<0) {
-  //     imgActual=arregloImg.length()-1;
-  //   }
-  // }
-  // let showImg = arregloImg[imgActual];
-  // contImgSlide.scr = `img/${showImg}`;
+function pasarImagen() {
+  let boton= this.id;
+  if (boton!=undefined) {
+    detenerSlide();
+    arrancarSlide();
+  }
+  if (boton=='btn-sig') {
+    imgActual++;
+    if(imgActual==arregloImg.length){
+      imgActual = 0;
+    }
+  } else {
+    imgActual--;
+    if (imgActual<0) {
+      imgActual=arregloImg.length-1;
+    }
+    }
+  let showImage = arregloImg[imgActual];
+  //console.log(imgActual);
+  //console.log(showImage);
+  contImgSlide.style = "background: url(img/"+showImage+");transition: 0.5s;";
+}
+
+function detenerSlide() {
+  clearInterval(tempo);
+}
+
+function arrancarSlide() {
+  tempo = setInterval(pasarImagen, 5000);
 }
